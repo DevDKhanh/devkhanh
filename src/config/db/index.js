03b776efb8c0c75d0bf95b31def555a4ccfbe95a
duykhanh <zerok49tt@gmail.com>
2021-08-12 18:14:24 +0700

    init app

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 async function connect(){
     try{
-        await mongoose.connect(process.env.DB_LOCAL, {
+        await mongoose.connect(process.env.DBMONGO_LOCAL_WEB_KHANH, {
             useNewUrlParser: true,
             useUnifiedTopology: true,
             useCreateIndex: true,
